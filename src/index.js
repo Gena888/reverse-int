e3295edb385f3fb14443f3d30fc062635a3e8d8d
Gena888 <theGena888@gmail.com>
2021-02-03 12:56:08 +0300

    feat: task ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n)
+    let numStr = num.toString()
+    let numArr = numStr.split('')
+    let numArrRev = numArr.reverse()
+    let newNum = numArrRev.join('')
+    
+    return newNum
 }
